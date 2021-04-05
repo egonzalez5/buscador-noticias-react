d53e7459c4104298ea62a2524abd05d1caab8e78
egonzalez5 <eduardo.gonzalez5599@gmail.com>
2021-04-05 16:52:28 -0400

    buscador de noticias

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
 
 function App() {
+
+  // definir la categoria y noticias
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      
+      const apiKey = "a546834d499d43c9a929c72a0b88a11c";
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=${apiKey}`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    }
+    consultarAPI();
+  }, [categoria]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+        <Header />
+
+        <div className="container white">
+            <Formulario 
+              guardarCategoria={guardarCategoria}
+            />
+
+            <ListadoNoticias 
+              noticias={noticias}
+            />
+        </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <nav className="nav-wrapper light-blue darken-3">
+            <a href="#!" className="brand-logo center">Buscador de Noticias</a>
+        </nav>
+     );
+}
+
+ 
+export default Header;
