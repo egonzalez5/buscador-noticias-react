1ab12ce14251c988db39eef95b5904ee6ef75a20
egonzalez5 <eduardo.gonzalez5599@gmail.com>
2021-04-05 17:34:53 -0400

    api

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Header = () => {
+const Header = ({}) => {
     return ( 
         <nav className="nav-wrapper light-blue darken-3">
             <a href="#!" className="brand-logo center">Buscador de Noticias</a>
